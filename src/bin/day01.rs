3449305de6a8031fc90002ec1ee51cf33ae2146a
jyli <jyli@dtu.dk>
2021-12-01 20:31:58 +0100

    update template

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,16 +1,20 @@
 use std::fs;
 
-fn part1(inp_vec: &Vec<i32>) -> u32 {
-    inp_vec
-        .windows(2)
+fn parse_input(filename: &str) -> Vec<i32> {
+    let contents = fs::read_to_string(filename).expect("can't find file");
+    let data: Vec<i32> = contents.lines().map(|s| s.parse().unwrap()).collect();
+    data
+}
+
+fn part1(data: &Vec<i32>) -> u32 {
+    data.windows(2)
         .map(|x| x[1] - x[0])
         .filter(|&x| x > 0)
         .count() as u32
 }
 
-fn part2(inp_vec: &Vec<i32>) -> u32 {
-    inp_vec
-        .windows(3)
+fn part2(data: &Vec<i32>) -> u32 {
+    data.windows(3)
         .map(|x| x[0] + x[1] + x[2])
         .collect::<Vec<i32>>()
         .windows(2)
@@ -21,14 +25,12 @@ fn part2(inp_vec: &Vec<i32>) -> u32 {
 
 fn main() {
     let now = std::time::Instant::now();
+    let data = parse_input("input/day01.txt");
 
-    let contents = fs::read_to_string("input/day01.txt").expect("can't find file");
-    let inp_vec: Vec<i32> = contents.lines().map(|s| s.parse().unwrap()).collect();
-
-    let ans_part1 = part1(&inp_vec);
+    let ans_part1 = part1(&data);
     println!("part1: {}", ans_part1);
 
-    let ans_part2 = part2(&inp_vec);
+    let ans_part2 = part2(&data);
     println!("part2: {}", ans_part2);
 
     let time = now.elapsed().as_micros();
--- src/bin/template.rs
+++ src/bin/template.rs
@@ -1,22 +1,28 @@
 use std::fs;
 
-fn part1() -> u32 {
+fn parse_input(filename: &str) -> Vec<i32> {
+    let contents = fs::read_to_string(filename).expect("can't find file");
+    let data: Vec<i32> = contents.lines().map(|s| s.parse().unwrap()).collect();
+    data
+}
+
+fn part1(data: &Vec<i32>) -> u32 {
     0
 }
 
-fn part2() -> u32 {
+fn part2(data: &Vec<i32>) -> u32 {
     0
 }
 
 fn main() {
     let now = std::time::Instant::now();
 
-    let contents = fs::read_to_string("input/day00.txt").expect("can't find file");
+    let data = parse_input("input/day00.txt");
 
-    let ans_part1 = part1();
+    let ans_part1 = part1(&data);
     println!("part1: {}", ans_part1);
 
-    let ans_part2 = part2();
+    let ans_part2 = part2(&data);
     println!("part2: {}", ans_part2);
 
     let time = now.elapsed().as_micros();
